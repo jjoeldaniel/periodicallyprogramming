4a8d41865e5425d617fe57898ee5fd9f18f8df50
Joel <100006388+joelrico@users.noreply.github.com>
2022-10-16 21:12:24 -0700

    now properly stores dark mode status

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 // Dark Mode is off by default
+window.onload = init;
 let dark = false;
 
-if (localStorage.getItem("dark_mode") === null) {
-    localStorage.setItem("dark_mode", "false");
-}
+function init() {
+    let element = document.getElementById("dark-mode-button");
+    element.addEventListener("click", dark_mode);
 
-if (localStorage.getItem("dark_mode") === "true") {
-    dark = true;
+    if (localStorage.getItem("dark_mode") === null) {
+        localStorage.setItem("dark_mode", "false");
+    }
+    
+    if (localStorage.getItem("dark_mode") === "true") {
+        dark = true;
+    }
+    else if (localStorage.getItem("dark_mode") === "false") {
+        dark = false;
+    }
 }
-else if (localStorage.getItem("dark_mode") === "false") {
-    dark = false;
-}
-
 
 // Toggle Dark Mode
 function dark_mode() {
     document.body.classList.toggle("projects-dark-mode")
     let buttons = document.getElementsByClassName("btn");
-    dark = !dark;
 
-    // light
+    // to light
     if (dark) {
-        document.getElementById("toggle").src= "https://github.com/jjoeldaniel/resume/blob/main/img/sun.png?raw=true";
+        document.getElementById("dark-mode-button").src= "https://raw.githubusercontent.com/jjoeldaniel/resume/main/img/dark-mode-button.png";
+        dark = false;
         localStorage.setItem("dark_mode", "false");
 
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].style.color = "#000000"; 
         }
     }
-    // dark
+    // to dark
     else {
-        document.getElementById("toggle").src= "https://raw.githubusercontent.com/jjoeldaniel/resume/main/img/dark-mode-button.png";
+        document.getElementById("dark-mode-button").src= "https://github.com/jjoeldaniel/resume/blob/main/img/sun.png?raw=true";
+        dark = true;
         localStorage.setItem("dark_mode", "true");
 
         for (let i = 0; i < buttons.length; i++) {
