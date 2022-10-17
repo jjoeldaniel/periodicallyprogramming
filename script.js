b6759f86867d0215baedafff56a30d04d214297b
Joel <100006388+joelrico@users.noreply.github.com>
2022-10-16 20:47:22 -0700

    fixed incorrect syntax

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let dark = false;
 
 if (localStorage.getItem("dark_mode") === null) {
-    localStorage.set("dark_mode", "false");
+    localStorage.setItem("dark_mode", "false");
 }
 
 if (localStorage.getItem("dark_mode") === "true") {
@@ -19,17 +19,19 @@ function dark_mode() {
     let buttons = document.getElementsByClassName("btn");
     dark = !dark;
 
+    // light
     if (dark) {
         document.getElementById("toggle").src= "https://github.com/jjoeldaniel/resume/blob/main/img/sun.png?raw=true";
-        localStorage.set("dark_mode", "false");
+        localStorage.setItem("dark_mode", "false");
 
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].style.color = "#000000"; 
         }
     }
+    // dark
     else {
         document.getElementById("toggle").src= "https://raw.githubusercontent.com/jjoeldaniel/resume/main/img/dark-mode-button.png";
-        localStorage.set("dark_mode", "true");
+        localStorage.setItem("dark_mode", "true");
 
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].style.color = "#FFFFFF"; 
