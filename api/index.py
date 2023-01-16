df08175a3590fd05e181bec80979c9fa316d2fac
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 15:55:04 -0800

    Fixed index

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+# Register blueprints
+# app.register_blueprint(api, url_prefix='/api')
 
 @app.route('/')
 def index():
