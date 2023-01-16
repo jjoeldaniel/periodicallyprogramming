7a4dc9bbfd636599f13ef65edd9fe578438381bb
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 15:43:14 -0800

    Uncommented api

--- api/api.py
+++ api/api.py
@@ -1,37 +1,37 @@
-# from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify
 
-# api = Blueprint('api', __name__)
+api = Blueprint('api', __name__)
 
-# @api.route('/api', methods=['GET'])
-# def req():
+@api.route('/api', methods=['GET'])
+def req():
 
-#     if request.method == 'GET':
+    if request.method == 'GET':
 
-#         # Get query
-#         q = request.args.get('query')
+        # Get query
+        q = request.args.get('query')
 
-#         # Check if query is empty
-#         if q == None:
-#             return 'Query is empty', 400
+        # Check if query is empty
+        if q == None:
+            return 'Query is empty', 400
 
-#         if q == 'projects':
+        if q == 'projects':
 
-#             return jsonify([
-#                 {
-#                     'name': 'Discord Bot',
-#                     'description': 'Multi-purpose Discord bot '
-#                 },
-#                 {
-#                     'name': 'Genius API Wrapper',
-#                     'description': 'Python wrapper for Genius API '
-#                 },
-#                 {
-#                     'name': 'Genius API Wrapper',
-#                     'description': 'A Discord bot meant to integrate with your Canvas class! '
-#                 }
+            return jsonify([
+                {
+                    'name': 'Discord Bot',
+                    'description': 'Multi-purpose Discord bot '
+                },
+                {
+                    'name': 'Genius API Wrapper',
+                    'description': 'Python wrapper for Genius API '
+                },
+                {
+                    'name': 'Genius API Wrapper',
+                    'description': 'A Discord bot meant to integrate with your Canvas class! '
+                }
 
-#             ])
+            ])
 
-#     # Throw error 405
-#     else:
-#         return 'Method not allowed', 405
+    # Throw error 405
+    else:
+        return 'Method not allowed', 405
--- api/index.py
+++ api/index.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-# from api import api
+from api import api
 
 app = Flask(__name__)
-# app.register_blueprint(api)
+app.register_blueprint(api)
 
 @app.route('/')
 def index():
-    # return render_template('index.html')
-    return 'Hello World'
+    return render_template('index.html')
