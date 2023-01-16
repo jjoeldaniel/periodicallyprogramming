7639ce33c42fa43474bf39ced7793a0b4e14877e
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 03:14:46 -0800

    Removed catch all

--- api/index.py
+++ api/index.py
@@ -7,10 +7,5 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 def index():
     return render_template('index.html')
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
-
 if __name__ == '__main__':
     app.run()
