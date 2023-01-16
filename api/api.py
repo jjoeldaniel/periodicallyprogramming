aadb2522288241818c5ac774f709fd321cc0de23
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 05:22:59 -0800

    Added more to API

--- api/api.py
+++ api/api.py
@@ -1,8 +1,37 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 api = Blueprint('api', __name__)
 
-@api.route('/api')
+@api.route('/api', methods=['GET'])
 def req():
-    user = request.args.get('user')
-    return f'Hello {user}'
+
+    if request.method == 'GET':
+
+        # Get query
+        q = request.args.get('query')
+
+        # Check if query is empty
+        if q == None:
+            return 'Query is empty', 400
+
+        if q == 'projects':
+
+            return jsonify([
+                {
+                    'name': 'Discord Bot',
+                    'description': 'Multi-purpose Discord bot '
+                },
+                {
+                    'name': 'Genius API Wrapper',
+                    'description': 'Python wrapper for Genius API '
+                },
+                {
+                    'name': 'Genius API Wrapper',
+                    'description': 'A Discord bot meant to integrate with your Canvas class! '
+                }
+
+            ])
+
+    # Throw error 405
+    else:
+        return 'Method not allowed', 405
