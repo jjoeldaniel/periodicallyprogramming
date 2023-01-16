e60a2fd5127806327dce6bea14eadc75527a403d
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 15:41:19 -0800

    Commented out api.py for debugging

---
+++ api/index.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+# from api import api
+
+app = Flask(__name__)
+# app.register_blueprint(api)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
