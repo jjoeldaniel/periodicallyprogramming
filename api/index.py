589cd8f563e6ebfe38c4683dbb6e142a33addce6
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 03:46:59 -0800

    Moved index.py

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from flask import *
-import json, time
-
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
---
+++ index.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
