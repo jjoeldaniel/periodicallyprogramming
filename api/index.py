64f4d7ef520cde917eeba72b23d68a1b832df749
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 03:40:23 -0800

    Fixed run

--- api/index.py
+++ api/index.py
@@ -6,7 +6,3 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 @app.route('/')
 def index():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    from vercel import import run_lambda
-    run_lambda(app)
