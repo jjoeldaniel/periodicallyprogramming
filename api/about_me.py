337e23dd23a2577aad0c740f844661e6fa43c8b5
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-17 01:58:10 -0800

    Added about_me.py, split into files

---
+++ api/about_me.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+
+about_me = Blueprint('about_me', __name__)
+
+@about_me.route('/api/about_me')
+def show():
+    return jsonify(
+        {
+            'name': 'Joel',
+            'description': 'Thats me!'
+        }), 200
--- api/index.py
+++ api/index.py
@@ -1,58 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+from about_me import about_me
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
-
-args = ['query']
+app.register_blueprint(about_me)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api')
+@app.route('/api/ping')
 def api():
-
-    # Check if request is empty
-    if request.args is None:
-        return jsonify(
-            {'status:' '400'},
-            {'error': 'No arguements provided'}), 400
-
-    # Check if all arguements are valid and return 400 if not
-    for arg in list(request.args.keys()):
-        if arg not in args:
-            return jsonify(
-                {'status': '400'},
-                {'error': 'Invalid arguement provided'}), 400
-
-    if request.method == 'GET':
-
-        # Get query
-        q = request.args.get('query')
-
-        if q == 'projects':
-
-            return jsonify([
-                {
-                    'name': 'Discord Bot',
-                    'description': 'Multi-purpose Discord bot '
-                },
-                {
-                    'name': 'Genius API Wrapper',
-                    'description': 'Python wrapper for Genius API '
-                },
-                {
-                    'name': 'Genius API Wrapper',
-                    'description': 'A Discord bot meant to integrate with your Canvas class! '
-                }
-
-            ], 200)
-        
-        else:
-            return jsonify(
-                {'status': '400'},
-                {'error': 'Invalid query provided'}), 400
-
-    # Throw error 405
-    return jsonify(
-                {'status': '405'},
-                {'error': 'Method not allowed'}), 405
+    return jsonify({'status': 'success'}), 200
+    
