984816cd4055da8dd465b9c7007f7f542425e015
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-17 02:45:07 -0800

    Commented out blueprint, changed import

--- api/index.py
+++ api/index.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, jsonify
-from about_me import about_me
+from about_me import *
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
-app.register_blueprint(about_me)
+# app.register_blueprint(about_me)
 
 
 @app.route('/')
