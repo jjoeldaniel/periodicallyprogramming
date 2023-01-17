ee2d5bd3138a81df4b9665ab3239108023782ce1
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-17 03:31:41 -0800

    Added doc explaining import

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, jsonify
+
+# Vercel imports require the leading directory for this to work
+# Assume all imports are relative to the root of the project
 from api.about_me import about_me
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
