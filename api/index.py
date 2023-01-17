a6321d87a13b82446b074b481837aea4c1719bf6
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-16 16:54:51 -0800

    Added additional checks

--- api/index.py
+++ api/index.py
@@ -2,20 +2,32 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
+args = ['query']
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/api')
 def api():
+
+    # Check if request is empty
+    if request.args is None:
+        return jsonify(
+            {'status:' '400'},
+            {'error': 'No arguements provided'}), 400
+
+    # Check if all arguements are valid and return 400 if not
+    for arg in list(request.args.keys()):
+        if arg not in args:
+            return jsonify(
+                {'status': '400'},
+                {'error': 'Invalid arguement provided'}), 400
+
     if request.method == 'GET':
 
         # Get query
         q = request.args.get('query')
-
-        # Check if query is empty
-        if q == None:
-            return 'Query is empty', 400
 
         if q == 'projects':
 
@@ -33,8 +45,14 @@ def api():
                     'description': 'A Discord bot meant to integrate with your Canvas class! '
                 }
 
-            ])
+            ], 200)
+        
+        else:
+            return jsonify(
+                {'status': '400'},
+                {'error': 'Invalid query provided'}), 400
 
     # Throw error 405
-    else:
-        return 'Method not allowed', 405
+    return jsonify(
+                {'status': '405'},
+                {'error': 'Method not allowed'}), 405
