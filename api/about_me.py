e33f72bb6a44eca18df4b906aa809f5e9aa0ad5c
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-17 02:56:07 -0800

    Revert import

--- api/about_me.py
+++ api/about_me.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, jsonify
 
-# Import app from index.py
-from api.index import app
-
-# about_me = Blueprint('about_me', __name__)
+about_me = Blueprint('about_me', __name__)
 
 
-@app.route('/api/about_me')
+@about_me.route('/api/about_me')
 def show():
     return jsonify(
         {
