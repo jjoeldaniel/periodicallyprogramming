c9fbbdfc69415507d97c6eb7df9d6595043ff1ea
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-20 11:43:59 -0800

    Removed about_me

--- api/index.py
+++ api/index.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, jsonify
 
-# Vercel imports require the leading directory for this to work
-# Assume all imports are relative to the root of the project
-from api.about_me import about_me
-
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
-app.register_blueprint(about_me)
 
 
 @app.route('/')
