7ba1be43cc553e51091adf0ff5715f63add50fea
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-21 23:02:45 -0700

    updated routes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect, url_for
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder='../templates',
+            static_folder='../static')
 
 
 @app.route('/')
 @app.route('/index')
+@app.rotue('/home')
 def index():
+    """Our default routes of '/' and '/index'
+    Return: The content we want to display to a user
+    """
+
     return render_template('index.html')
 
 
@@ -47,3 +53,19 @@ def projects():
 @app.route('/api/ping')
 def api():
     return jsonify({'status': 'success'}), 200
+
+
+@app.route('/<path:path>')
+def catch_all(path):
+    """A special route that catches all other requests
+    Note: Let this be your last route. Priority is defined
+    by order, so placing this above other functions will
+    cause catch_all() to override then.
+    Return: A redirect to our index route
+    """
+
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
