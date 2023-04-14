d61c4adfdc613ad934359d4167f63301bc2d2e87
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-14 10:18:23 +0000

    optomized blog loading

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import markdown
@@ -14,16 +13,42 @@ templates = Jinja2Templates(directory="templates")
 
 
 # Get all files and sort by most recently modified
-posts = list()
-for post in os.listdir('./blogs'):
-    posts.append(post)
-posts = sorted(posts, key=lambda f: os.path.getmtime(os.path.join('./blogs', f)), reverse=True)
+blog_posts = dict()
+blog_metadata = list()
+blog_html = dict()
 
-# Build list of file metadata
-blog_posts = list()
-for post in posts:
-    blog_posts.append({'name': f'{str(post).replace(".md", "").replace("_", " ")}',
-                 'href': f'{str(post)}', 'file_name': f'{str(post).replace(".md", "")}'})
+def initialize_blog(blog_posts: dict, blog_metadata: list, blog_html: dict):
+    for post in os.listdir('./blogs'):
+
+        with open(f'./blogs/{post}') as f:
+            markdown_content = f.read()
+
+        md = markdown.Markdown(extensions=['meta', 'fenced_code', 'codehilite'])
+        output = md.convert(markdown_content)
+        meta = md.Meta
+
+        formatter = HtmlFormatter(
+            style='github-dark', full=True, cssclass="codehilite")
+        css_string = formatter.get_style_defs()
+        html = f"<style> {css_string} </style> {output}"
+
+        title = "".join(meta['title'][0])
+        date = "".join(meta['date'][0])
+        file_name = str(post).replace('.md', '')
+
+        blog_html[file_name] = html
+
+        blog_metadata.append({'name': title,
+                           'href': f'{str(post)}', 'date': date, 'file_name': file_name})
+
+        blog_posts[file_name] = ({'name': title,
+                           'href': f'{str(post)}', 'date': date, 'file_name': file_name})
+
+    # Sort by most recent
+    blog_metadata = sorted(blog_metadata, key=lambda x: x['date'], reverse=True)
+
+
+initialize_blog(blog_posts, blog_metadata, blog_html)
 
 
 @app.get('/index', response_class=HTMLResponse)
@@ -39,30 +64,15 @@ async def home(request: Request):
 
 @app.get('/blog', response_class=HTMLResponse)
 async def blog_index(request: Request):
-    return templates.TemplateResponse('blog_index.html', {'request': request, 'blog_posts': blog_posts})
+    return templates.TemplateResponse('blog_index.html', {'request': request, 'blog_posts': blog_metadata})
 
 
 @app.get('/blog/{title}')
 async def blog_post(title: str, request: Request):
-    blog_posts = [str(post) for post in os.listdir('./blogs')]
+    if title not in blog_html:
+        raise HTTPException(status_code=404, detail=f'Blog with title \"{title}\" not found')
 
-    if str(title)+".md" not in blog_posts:
-        return RedirectResponse(url='/blog')
-
-    with open(f'./blogs/{title}.md') as f:
-        markdown_content = f.read()
-
-    md = markdown.Markdown(extensions=['meta', 'fenced_code', 'codehilite'])
-    output = md.convert(markdown_content)
-    meta = md.Meta
-    print(meta)
-
-    formatter = HtmlFormatter(
-        style='github-dark', full=True, cssclass="codehilite")
-    css_string = formatter.get_style_defs()
-    html = f"<style> {css_string} </style> {output}"
-
-    return templates.TemplateResponse('blog.html', {'request': request, 'html': html, 'title': meta['title'][0]})
+    return templates.TemplateResponse('blog.html', {'request': request, 'html': blog_html[title], 'title': blog_posts[title]['name']})
 
 
 @app.get('/about', response_class=HTMLResponse)
