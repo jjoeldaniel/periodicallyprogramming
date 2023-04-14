41e0079d976a0dc79b9f29b2a1fb26cb100dc4d2
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-13 21:49:46 -0700

    switch to fastapi

---
+++ main.py
@@ -0,0 +1,109 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import markdown
+import markdown.extensions.fenced_code
+from pygments.formatters import HtmlFormatter
+import os
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/index', response_class=HTMLResponse)
+@app.get('/home', response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
+async def home(request: Request):
+    """Our default routes of '/' and '/index'
+    Return: The content we want to display to a user
+    """
+
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/blog', response_class=HTMLResponse)
+def blog_index(request: Request):
+
+    # Get all files and sort by most recently modified
+    posts = list()
+    for post in os.listdir('./blogs'):
+        posts.append(post)
+    posts = sorted(posts, key=lambda f: os.path.getmtime(os.path.join('./blogs', f)), reverse=True)
+
+    # Build list of file metadata
+    blog_posts = list()
+    for post in posts:
+        blog_posts.append({'name': f'{str(post).replace(".md", "").replace("_", " ")}',
+                     'href': f'{str(post)}', 'file_name': f'{str(post).replace(".md", "")}'})
+
+    print(blog_posts)
+    return templates.TemplateResponse('blog_index.html', {'request': request, 'blog_posts': blog_posts})
+
+
+@app.get('/blog/{title}')
+def blog_post(title: str, request: Request):
+    blog_posts = [str(post) for post in os.listdir('./blogs')]
+
+    if str(title)+".md" not in blog_posts:
+        return RedirectResponse(url='/blog')
+
+    with open(f'./blogs/{title}.md') as f:
+        markdown_content = f.read()
+
+    html_content = markdown.markdown(
+        markdown_content,
+        extensions=["fenced_code", "codehilite"]
+    )
+
+    formatter = HtmlFormatter(
+        style='github-dark', full=True, cssclass="codehilite")
+    css_string = formatter.get_style_defs()
+    md_css_string = "<style>" + css_string + "</style>"
+    md_template = md_css_string + html_content
+
+    title = str(title).replace(".md", "").replace("_", " ")
+    return templates.TemplateResponse('blog.html', {'request': request, 'html': md_template, 'title': title})
+
+
+@app.get('/about', response_class=HTMLResponse)
+@app.get('/contact', response_class=HTMLResponse)
+async def contact(request: Request):
+    return templates.TemplateResponse('contact.html', {'request': request})
+
+
+@app.get('/project', response_class=HTMLResponse)
+@app.get('/projects', response_class=HTMLResponse)
+async def projects(request: Request):
+    return templates.TemplateResponse('projects.html', {'request': request})
+
+
+@app.get('/github')
+async def github():
+    return RedirectResponse(url='https://github.com/jjoeldaniel')
+
+
+@app.get('/resume')
+async def resume():
+    return RedirectResponse(url='https://www.periodicallyprogramming.com/static/resume.pdf')
+
+
+@app.get('/source')
+async def source():
+    return RedirectResponse(url='https://github.com/jjoeldaniel/periodicallyprogramming')
+
+
+@app.get('/linkedin')
+async def linkedin():
+    return RedirectResponse(url='https://www.linkedin.com/in/joeldanielrico')
+
+
+@app.get('/*')
+async def catch_all():
+    return RedirectResponse(url="/home")
+
+
+if __name__ == '__main__':
+    app.run()
