c311f55ba2caa7f4ab7d24e72887ae1f1099eba8
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-05 04:14:10 -0700

    added autocomplete

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,4 +1,10 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { Autocomplete } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+	import { redirect } from '@sveltejs/kit';
+	import { prevent_default } from 'svelte/internal';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
 
@@ -6,15 +12,36 @@
 	const posts = data.posts.posts;
 	
 	// convert posts to array
-	// @ts-ignore
-	let arr = Object.values(posts);
+	const arr = Object.values(posts);
+
+	// array of post names
+	const postNames = arr.map((post: any) => post.title);
+
+	// build autocomplete options
+	let query = '';
+	let flavorOptions: AutocompleteOption[] = [];
+
+	arr.forEach((post: any) => {
+		flavorOptions.push({
+			label: post.title,
+			value: post.slug
+		});
+	});
+
+	function onFlavorSelection(event: any): void {
+		query = event.detail.label;
+	}	
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-8">
 		<h1>Welcome to my blog!</h1>
 		
-		<input class="input" type="search" name="blogSearch" placeholder="Search..." />
+		<input class="input" bind:value={query} type="search" name="blogSearch" placeholder="Search..." />
+
+		<div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto">
+			<Autocomplete bind:input={query} options={flavorOptions} on:selection={onFlavorSelection} />
+		</div>
 
 		<div class="space-y-3">
 			<h2>Recent Posts</h2>
