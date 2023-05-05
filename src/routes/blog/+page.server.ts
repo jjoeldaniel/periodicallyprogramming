712c3305df0633a28296177dadf5c2ec90b94eec
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-05 02:44:48 -0700

    added WIP blog loading

---
+++ src/routes/blog/+page.server.ts
@@ -0,0 +1,18 @@
+/** @type {import('./$types').PageServerLoad} */
+export function load() {
+  const posts: any[] = [];
+  // TODO: Return all blog posts
+  // as an array of objects with
+  // the following shape:
+  //
+  // {
+  //   slug: string;
+  //   title: string;
+  //   date: string;
+  //   content: string;
+  // } 
+  return {
+    status: 200,
+    posts
+  };
+}
