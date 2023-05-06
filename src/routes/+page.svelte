5df2c71c919705e2a8c74138398d54226d93bc9d
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 06:24:06 -0700

    add root page padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto max-w-xl flex justify-center items-center">
-	<div class="space-y-5">
+	<div class="space-y-5 mx-5">
 		<h1><strong>Joel Daniel Rico</strong></h1>
 		<h4><i>Aspiring Software Engineer</i></h4>
 
