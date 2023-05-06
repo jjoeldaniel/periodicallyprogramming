45b938a185174246bfa79bdfd40bef84b56ebdb9
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 04:07:06 -0700

    removed /about

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -10,7 +10,6 @@
 	<ul>
 		<li class="hidden"><a href="/">easter egg</a></li>
 		<li><a href="/" on:click={drawerClose}>Home</a></li>
-		<li><a href="/about" on:click={drawerClose}>About</a></li>
 		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
 		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
 		<li>
