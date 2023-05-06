aca8a0a3aa6050df4665108efca3a3dda4c27116
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 01:54:29 -0700

    now extracts metadata

--- src/routes/blog/[blogName]/+page.svelte
+++ src/routes/blog/[blogName]/+page.svelte
@@ -1,17 +1,24 @@
 <script>
 	import BlogHeader from '$lib/BlogHeader.svelte';
 	import MarkdownIt from 'markdown-it';
+	import meta from 'markdown-it-meta'
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
     let md = new MarkdownIt();
+	md.use(meta);
+
 	const html = md.render(data.content);
+	const title = md.meta.title;
+	const date = new Date(md.meta.date).toLocaleDateString('en-US');
+
+	console.log(md.meta);
 </script>
 
 <div class="container h-full mx-auto flex flex-col justify-center">
 	<div class="space-y-5">
-		<BlogHeader title={data.title}/>
+		<BlogHeader title={title} date={date}/>
 		{@html html}
     </div>
 </div>
