00b8ae9b83af80fd18aa7c349ba57c8cc31b008d
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 01:54:24 -0700

    added date

--- src/lib/BlogHeader.svelte
+++ src/lib/BlogHeader.svelte
@@ -1,9 +1,11 @@
 <script lang='ts'>
     import { Avatar } from '@skeletonlabs/skeleton';
     export let title: string = "Title";
+    export let date: string = "Date";
 </script>
 
 <a href="https://github.com/jjoeldaniel">
     <Avatar class="mx-auto" src="https://avatars.githubusercontent.com/u/100006388?v=4" width="w-32" rounded="rounded-full" />
 </a>
 <h1 class="text-center">{title}</h1>
+<p class="text-center">{date}</p>
