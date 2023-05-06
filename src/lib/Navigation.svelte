e7b7212d427a20af592c54f512dc6cd30f401e6f
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 03:22:52 -0700

    ninja li

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,22 +1,20 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { drawerStore } from '@skeletonlabs/skeleton';
 
 	function drawerClose(): void {
 		drawerStore.close();
 	}
-
-	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');
 </script>
 
 <nav class="list-nav">
 	<ul>
-		<li><a href="/" class={classesActive('/about')} on:click={drawerClose}>Home</a></li>
-		<li><a href="/about" class={classesActive('/about')} on:click={drawerClose}>About</a></li>
-		<li><a href="/blog" class={classesActive('/blog')} on:click={drawerClose}>Blog</a></li>
-		<li><a href="/contact" class={classesActive('/contact')} on:click={drawerClose}>Contact</a></li>
+		<li class="hidden"><a href="/">easter egg</a></li>
+		<li><a href="/" on:click={drawerClose}>Home</a></li>
+		<li><a href="/about" on:click={drawerClose}>About</a></li>
+		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
+		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
 		<li>
-			<a href="/projects" class={classesActive('/projects')} on:click={drawerClose}>Projects</a>
+			<a href="/projects" on:click={drawerClose}>Projects</a>
 		</li>
 	</ul>
 </nav>
