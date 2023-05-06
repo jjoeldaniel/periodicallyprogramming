8099fe8ea5c6886fe5024a281cf22fdd1a6c1609
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-05 17:37:43 -0700

    fix sidebarLeft

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,7 +41,7 @@
 			<ul>
 				<li><a href="/">Home</a></li>
 				<li><a href="/about">About</a></li>
-				<li><a href="blog">Blog</a></li>
+				<li><a href="/blog">Blog</a></li>
 				<li><a href="/contact">Contact</a></li>
 				<li><a href="/projects">Projects</a></li>
 			</ul>
