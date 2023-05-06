d7ed1a689640041196f58f29252d06847a47391b
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 06:58:13 -0700

    removed duplicate imports

--- src/routes/blog/[blogName]/+layout.svelte
+++ src/routes/blog/[blogName]/+layout.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-	import '@skeletonlabs/skeleton/styles/all.css';
-	// Most of your app wide CSS should be put in this file
-	import '../../../app.postcss';
 	import { TableOfContents } from '@skeletonlabs/skeleton';
 	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
