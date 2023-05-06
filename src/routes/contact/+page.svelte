86f704af504b02d7ed71819c6a1d7be48f357a06
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 06:39:08 -0700

    added button for copying Discord ID

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import BlogHeader from '$lib/BlogHeader.svelte';
 	import { clipboard } from '@skeletonlabs/skeleton';
+	import { Toast, toastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
+
+	function copyToClipboard() {
+		const t: ToastSettings = {
+			message: 'Copied Discord ID to clipboard!'
+		};
+		toastStore.trigger(t);
+	}
 </script>
+
+<Toast />
 
 <div class="container h-full mx-auto flex flex-col justify-center text-center">
 	<div class="space-y-5">
@@ -13,7 +24,11 @@
 			<li>GitHub: <a href="https://github.com/jjoeldaniel">jjoeldaniel</a></li>
 			<li>
 				Discord:
-				<button class="text-blue-500 hover:text-blue-700" use:clipboard={'joel#0005'}>
+				<button
+					class="text-blue-500 hover:text-blue-700"
+					on:click={copyToClipboard}
+					use:clipboard={'joel#0005'}
+				>
 					joel#0005
 				</button>
 			</li>
