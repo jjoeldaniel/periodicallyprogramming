8b1bf09a7ea7237090e5147e286db79a4fcb6168
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-06 05:16:02 -0700

    add layout

---
+++ src/routes/blog/[blogName]/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../../../app.postcss';
+	import { TableOfContents } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="sidebarRight">
+		<div class="2xl:flex hidden py-20 px-3 fixed top-0 right-0">
+			<TableOfContents target="#page" />
+		</div>
+	</svelte:fragment>
+	<slot />
+</AppShell>
