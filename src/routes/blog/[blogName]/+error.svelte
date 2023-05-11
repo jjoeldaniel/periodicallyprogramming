4180440f429e8792a8b08a4d1377615aea3bb656
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-11 02:36:45 -0700

    added WIP redirect

--- src/routes/blog/[blogName]/+error.svelte
+++ src/routes/blog/[blogName]/+error.svelte
@@ -1,15 +1,19 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
+
+	let message: string = 'An error has occurred!';
+	let timer: number = 5;
+	if ($page.error != null) message = $page.error.message;
 </script>
 
 <div class="container h-full space-y-10 mx-auto max-w-xl flex flex-col justify-center items-center">
 	<div class="flex flex-row">
-		<h1>{$page.error.message}</h1>
+		<h1>{message}</h1>
 		<ProgressRadial />
 	</div>
 
 	<div>
-		<h5>Redirecting you back home...</h5>
+		<h5>Redirecting you back home in {timer} seconds...</h5>
 	</div>
 </div>
