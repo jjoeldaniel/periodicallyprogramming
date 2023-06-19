818f0e8c93efe21aa455163d4796fcd4fe364c9c
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-19 05:58:34 -0700

    new desktop navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,48 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar, LightSwitch, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 	import Navigation from '$lib/Navigation.svelte';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
 
 	function drawerOpen(): void {
 		drawerStore.open({
 			padding: 'p-3',
-			width: 'md:w-1/3 xl:w-1/6 lg:w-3/12 xl:w-3/12 w-8/12',
+			width: 'md:w-1/3 xl:w-1/6',
 			rounded: 'rounded-lg'
 		});
 	}
+
+	let tabsBottomNav = 0;
+	let path = $page.url.pathname;
+	switch (path) {
+		case '/':
+			tabsBottomNav = 0;
+			break;
+		case '/blog':
+			tabsBottomNav = 1;
+			break;
+		case '/projects':
+			tabsBottomNav = 2;
+			break;
+		case '/contact':
+			tabsBottomNav = 3;
+			break;
+		default:
+			tabsBottomNav = 0;
+	}
+
+	function buttonClick(event: MouseEvent): void {
+		let targetPath = event.target.name;
+		goto(targetPath);
+	}
 </script>
+
+<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+/>
 
 <Drawer>
 	<Navigation />
@@ -24,7 +56,7 @@
 	<svelte:fragment slot="header">
 		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 			<svelte:fragment slot="lead"
-				><button class="btn btn-sm mr-4" on:click={drawerOpen}>
+				><button class="btn btn-sm mr-4 block lg:hidden" on:click={drawerOpen}>
 					<span>
 						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
 							<rect width="100" height="20" />
@@ -32,23 +64,48 @@
 							<rect y="60" width="100" height="20" />
 						</svg>
 					</span>
-				</button></svelte:fragment
-			>
+				</button>
+				<div class="hidden lg:block">
+					<TabGroup
+						justify="justify-center"
+						active="variant-filled-primary"
+						hover="hover:variant-soft-primary"
+						flex="flex-1 lg:flex-none"
+						rounded=""
+						border=""
+						class="bg-surface-100-800-token w-full"
+					>
+						<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="home" value={0}>
+							<svelte:fragment slot="lead"
+								><span class="material-symbols-outlined"> home </span></svelte:fragment
+							>
+							Home
+						</Tab>
+						<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="blog" value={1}>
+							<svelte:fragment slot="lead"
+								><span class="material-symbols-outlined"> article </span></svelte:fragment
+							>
+							Blog
+						</Tab>
+						<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="projects" value={2}>
+							<svelte:fragment slot="lead"
+								><span class="material-symbols-outlined"> trophy </span></svelte:fragment
+							>
+							Projects
+						</Tab>
+						<Tab on:click={buttonClick} bind:group={tabsBottomNav} name="contact" value={3}>
+							<svelte:fragment slot="lead"
+								><span class="material-symbols-outlined"> chat </span></svelte:fragment
+							>
+							Contact
+						</Tab>
+					</TabGroup>
+				</div>
+			</svelte:fragment>
 			<h4 class="font-bold gradient-heading"><a href="/">periodicallyprogramming</a></h4>
 			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 
 	<slot />
-
-	<svelte:fragment slot="pageFooter">
-		<hr class="h-px mt-6 mb-6 border-0" />
-		<ul class="ul pb-5 flex space-x-7 justify-center">
-			<li class="li"><a href="https://github.com/jjoeldaniel/">GitHub</a></li>
-			<li class="li"><a href="/contact">Contact</a></li>
-			<li class="li">
-				<a href="https://github.com/jjoeldaniel/periodicallyprogramming">Source Code</a>
-			</li>
-		</ul></svelte:fragment
-	>
 </AppShell>
