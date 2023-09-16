7c557a0ecd28e2c36d278b3f9a789195f44d3258
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-16 01:22:36 -0700

    update links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    redirects:
+        {
+            "/github": "https://github.com/jjoeldaniel",
+            "/resume": "https://github.com/jjoeldaniel/resume/blob/main/resume.pdf",
+            "/linkedin": "https://www.linkedin.com/in/joeldanielrico",
+            "/mastadon": "https://mastodon.social/@jjoel",
+        }
 });
