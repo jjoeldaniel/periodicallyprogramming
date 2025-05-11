1d30d53d312c2278c8eb81766840777b00889932
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2025-05-11 01:34:27 -0700

    upgrade to tailwind4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    integrations: [tailwind()],
+  output: 'static',
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
